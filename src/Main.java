80274b9160c2b5d76807d4593f02eb0728773ee2
Paul-Bel <paul.bel.job@gmail.com>
2025-02-18 19:57:58 -0500

    small refactor

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,18 @@ public class Main {
         Scanner typeTask = new Scanner(System.in);
         int answer = 9;
 
-        System.out.println("Enter the number of tasks you want to perform.\n" +
-                "Task 1: What is your name\n" +
-                "Task 2: Multiplication table\n" +
-                "Task 3: Your language\n" +
-                "Task 4: Learn algebra");
-
         Set<Integer> validValues = new HashSet<>();
         validValues.add(0);
 
         while (!validValues.contains(answer)) {
-            System.out.print("Please enter a number: 1 or 2 or 3 or 4.\n" +
-                    "For exit press 0: ");
+            System.out.println(
+                    "Enter the number of tasks you want to perform." +
+                            "\nTask 1: What is your name" +
+                            "\nTask 2: Multiplication table" +
+                            "\nTask 3: Your language" +
+                            "\nTask 4: Learn algebra");
+            System.out.print("Please enter the number: 1 or 2 or 3 or 4." +
+                    "\nFor exit press 0: ");
 
             if (typeTask.hasNextInt()) {
                 answer = typeTask.nextInt();
@@ -33,11 +33,11 @@ public class Main {
                     case 3 -> studentLanguage();
                     case 4 -> taskAlgebra(0);
                 }
-            }
-            else {
+            } else {
                 typeTask.next();
-                System.out.println("You entered an incorrect value.\n" +
-                        "Please try again.");
+                System.out.println("________________________" +
+                        "\nYou entered an incorrect value." +
+                        "\nPlease try again.");
             }
         }
     }
